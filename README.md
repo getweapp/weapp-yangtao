14d250fdf5e89368dbb00b396230a44ebdedd649
getweapp <yw@getweapp.com>
2016-11-30 18:19:35 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,27 @@
+# 微信小程序－阳淘
+
+### 说明：
+
+实现品牌搜索和展示品牌商品列表功能。
+
+### 数据接口:
+
+- https://api.getweapp.com/vendor/lightstao/searchkey
+- https://api.getweapp.com/vendor/lightstao/product/search
+
+### 目录结构：
+
+- pages — 存放项目页面文件
+- utils — 存放项目页面文件
+
+### 开发环境：
+
+微信web开发者工具 v0.11.112301
+
+### 项目截图：
+
+https://www.getweapp.com/project?projectId=583ea793e8ff074c22472f47
+
+### 感谢：
+
+本项目原始版本由jackyshan提供：https://github.com/jackyshan/yangtaowechatapp
---
+++ pages/searchs/searchs.js
@@ -0,0 +1,69 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: '阳淘',
+    userInfo: {},
+    rcmdArr: []
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  },
+  bindKeyInput: function(e) {
+    var value = e.detail.value
+    console.log(value)
+
+    var that = this
+
+    wx.request({
+      url: 'https://api.getweapp.com/vendor/lightstao/searchkey',
+      data: {
+        key: value,
+      },
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header
+      header: {
+          'Content-Type': 'application/json'
+      },
+      success: function(res){
+        console.log('data:',res.data)
+        // success
+        console.log(Array.from(res.data.data))
+        that.setData({
+          rcmdArr:Array.from(res.data.data)
+        })
+      },
+      fail: function(res) {
+        // fail
+        console.log('失败了')
+        console.log(res.errMsg)
+      },
+      complete: function() {
+        // complete
+        console.log('结束了')
+      }
+    })
+    
+  },
+  goToPList: function(e) {
+    console.log(e.currentTarget.id)
+    wx.navigateTo ({
+      url: '../productList/productList?searchName='+e.currentTarget.id
+    })
+  }
+})
